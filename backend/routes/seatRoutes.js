3e71dba59645cb184ce4d2eac79edcfde2160b70
Pallavi bobde <pallavibobde@Pallavis-MacBook-Pro.local>
2024-09-29 23:21:26 +0530

    hey

--- backend/routes/seatRoutes.js
+++ backend/routes/seatRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { initializeSeats, getSeats, bookSeats } = require('../controllers/seatController');
 const router = express.Router();
 
-router.get('/initialize', initializeSeats); // Initialize seat data
+// router.get('/initialize', initializeSeats); // Initialize seat data
 router.get('/', getSeats); // Fetch all seats
 router.post('/book', bookSeats); // Book seats
 
